321b97dcb54bd9c8416648009cb271e969da5fa4
FontPsych <tlanno@itcollege.ee>
2016-05-18 17:23:25 +0300

    Add project files.

---
+++ DAL/DatabaseInitializer.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using Domain.Models;
+using Domain.Identity;
+using Microsoft.AspNet.Identity;
+using Newtonsoft.Json.Linq;
+
+namespace DAL
+{
+    //    public class DatabaseInitializer : DropCreateDatabaseIfModelChanges<DataBaseContext>
+    public class DatabaseInitializer : DropCreateDatabaseIfModelChanges<DataBaseContext>
+    {
+        protected override void Seed(DataBaseContext context)
+        {
+            var autoDetectChangesEnabled = context.Configuration.AutoDetectChangesEnabled;
+            // much-much faster for bulk inserts!!!!
+            context.Configuration.AutoDetectChangesEnabled = false;
+
+            SeedIdentity(context);
+            SeedArticles(context);
+
+
+            // restore state
+            context.Configuration.AutoDetectChangesEnabled = autoDetectChangesEnabled;
+
+            base.Seed(context);
+        }
+
+        private void SeedArticles(DataBaseContext context)
+        {
+            var articleHeadLine = "<h1>ASP.NET</h1>";
+            var articleBody =
+                "<p class=\"lead\">ASP.NET is a free web framework for building great Web sites and Web applications using HTML, CSS, and JavaScript.<br/>" +
+                "As a demo, here is simple Contact application - log in and save your contacts!</p>";
+            var article = new Article()
+            {
+                ArticleName = "HomeIndex",
+                ArticleHeadline =
+                    new MultiLangString(articleHeadLine, "en", articleHeadLine, "Article.HomeIndex.ArticleHeadline"),
+                ArticleBody = new MultiLangString(articleBody, "en", articleBody, "Article.HomeIndex.ArticleBody")
+            };
+            context.Articles.Add(article);
+            context.SaveChanges();
+
+            context.Translations.Add(new Translation()
+            {
+                Value = "<h1>ASP.NET on suurepärane!</h1>",
+                Culture = "et",
+                MultiLangString = article.ArticleHeadline
+            });
+
+            context.Translations.Add(new Translation()
+            {
+                Value =
+                    "<p class=\"lead\">ASP.NET on tasuta veebiraamistik suurepäraste veebide arendamiseks kasutades HTML-i, CSS-i, ja JavaScript-i.<br/>" +
+                    "Demona on siin lihtne Kontaktirakendus - logi sisse ja salvesta enda kontakte</p>",
+                Culture = "et",
+                MultiLangString = article.ArticleBody
+            });
+            context.SaveChanges();
+        }
+
+        private void SeedIdentity(DataBaseContext context)
+        {
+            var pwdHasher = new PasswordHasher();
+
+            // Roles
+            context.RolesInt.Add(new RoleInt()
+            {
+                Name = "Admin"
+            });
+
+            context.SaveChanges();
+
+            // Users
+            context.UsersInt.Add(new UserInt()
+            {
+                UserName = "admin",
+                Email = "lebo@lebo.ee",
+                FirstName = "Das",
+                LastName = "User",
+                PasswordHash = pwdHasher.HashPassword("admin"),
+                SecurityStamp = Guid.NewGuid().ToString()
+            });
+
+            context.SaveChanges();
+
+            // Users in Roles
+            context.UserRolesInt.Add(new UserRoleInt()
+            {
+                User = context.UsersInt.FirstOrDefault(a => a.UserName == "admin"),
+                Role = context.RolesInt.FirstOrDefault(a => a.Name == "Admin")
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
