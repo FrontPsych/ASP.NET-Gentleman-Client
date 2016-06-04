33b471cd8c4ad385a132751583612d608117324b
FontPsych <tlanno@itcollege.ee>
2016-06-04 11:52:19 +0300

    Added system logs view for admin role.

--- Web/Controllers/AdminController.cs
+++ Web/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -29,6 +30,58 @@ namespace Web.Controllers
             };
 
             return View(vm);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult SystemLogs(string logFileName)
+        {
+            var logPath = Server.MapPath("~") + @"\logs\";
+
+            var fileEntries =
+                Directory.GetFiles(logPath)
+                    .ToList()
+                    .Select(p => new { FileNameId = Path.GetFileName(p), FileName = Path.GetFileName(p) })
+                    .OrderByDescending(a => a.FileName)
+                    .ToList();
+            var e = fileEntries.FirstOrDefault();
+            var vm = new SystemLogIndexViewModel();
+            vm.LogFileSelectList = logFileName == null
+                ? new SelectList(fileEntries, nameof(e.FileNameId), nameof(e.FileName))
+                : new SelectList(fileEntries, nameof(e.FileNameId), nameof(e.FileName), logFileName);
+
+            logFileName = logPath + logFileName?.Trim();
+
+            if (System.IO.File.Exists(logFileName))
+            {
+                vm.LogFileContent = System.IO.File.ReadAllText(logFileName);
+            }
+
+            return View(vm);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult DeleteAllLogFiles()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost, ActionName("DeleteAllLogFiles")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteAllLogFilesConfirmed()
+        {
+
+            var logPath = Server.MapPath("~") + @"\logs\";
+
+            var fileEntries =
+                Directory.GetFiles(logPath);
+
+            foreach (var fileEntry in fileEntries)
+            {
+                System.IO.File.Delete(fileEntry);
+            }
+
+            return Redirect(nameof(Index));
         }
 
         public ActionResult About()
---
+++ Web/ViewModels/SystemLogViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.ViewModels
+{
+    public class SystemLogIndexViewModel
+    {
+        public string LogFileName { get; set; }
+        public SelectList LogFileSelectList { get; set; }
+
+        public string LogFileContent { get; set; }
+    }
+}
